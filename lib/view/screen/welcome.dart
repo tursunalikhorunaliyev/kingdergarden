516fabf608e2a1c955c7df3a8ed766cfb6a78481
usmonaliyevm <uzayyub2007@gmail.com>
2023-04-25 16:50:14 +0500

    okkkk

--- lib/view/screen/welcome.dart
+++ lib/view/screen/welcome.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:kingdergarden/view/custome/witgets.dart';
 
 class Welcome extends StatelessWidget {
   const Welcome({super.key});
@@ -8,11 +9,16 @@ class Welcome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Image.asset(
-          "assets/images/first.png",
-        ),
+      body: Column(
+        children: [
+          Center(
+            child: Image.asset(
+              "assets/images/first.png",
+            ),
+          ),
+          Next(),
+        ],
       ),
     );
   }
-}
+} //https://www.figma.com/file/OPwZeBzxbLlWxH0AzFYEY6/Untitled?node-id=1-447&t=QSAcWTKtKuSvVmdL-0
