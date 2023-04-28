dd32ce1015ec0f99623762eda0fae91c476ab6bc
RahmatullohXolmatov <rholmatov151@gmail.com>
2023-04-28 01:48:33 -0700

    commenttt

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:kingdergarden/view/screen/enter_profile.dart';
-import 'package:kingdergarden/view/screen/welcome.dart';
+import 'package:kingdergarden/view/screen/test.dart';
 
 void main(List<String> args) {
-  runApp(const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: EnterProfile(),
-  ));
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Test(),
+    ),
+  );
 }
